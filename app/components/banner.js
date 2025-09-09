52e96f047e44dd385216dd0fc2e98df59fd947a4
Yuvem <matjuve33@icloud.com>
2025-09-09 10:01:10 +0200

    accessibilité

--- app/components/banner.js
+++ app/components/banner.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Banner = () => (
 <header className="flex justify-between">
-    <div className="h-12 relative w-52">
-    <Image src="/logo.svg" fill alt="Logo fisheye" className="object-contain"/>
-    </div>
+    <Link href="/" className="h-12 relative w-52">
+    <Image src="/logo.svg" fill alt="Fisheye Home Page" className="object-contain"/>
+    </Link>
     <h1 className="font-normal text-4xl text-[var(--main-color)]">Nos photographes</h1>
 </header>
 )
--- app/components/photoghrapherCard.js
+++ app/components/photoghrapherCard.js
@@ -1,17 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 
-const PhotographerCard = ({photographer}) => (
-  <article className="flex flex-col items-center  w-1/3" >
-    <div className="flex flex-col items-center">
-        <div className="h-52 w-50 relative overflow-hidden rounded-full">
-              <Image src={`/${photographer.portrait}`} fill alt={`${photographer.name}`} className="object-cover overflow-visible" />
-    </div>
-    <div className="flex flex-col items-center">
+const PhotographerCard = ({ photographer }) => (
+  <article className="flex flex-col items-center  w-1/3">
+    <Link href="#">
+      <div className="h-52 w-50 relative overflow-hidden rounded-full">
+        <Image src={`/${photographer.portrait}`} fill alt="" className="object-cover overflow-visible" />
+      </div>
       <h2>{photographer.name}</h2>
-      <h3>{photographer.city}, {photographer.country}</h3>
+    </Link>
+
+    <div className="flex flex-col items-center">
+      <p className="text-base text-[var(--main-color)]">
+        {photographer.city}, {photographer.country}
+      </p>
       <p className="text-xs">{photographer.tagline}</p>
       <p className="text-[#757575] text-xs">{photographer.price}€/jour</p>
-    </div>
     </div>
   </article>
 );
